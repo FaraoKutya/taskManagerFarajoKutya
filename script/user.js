7b512bb4137b2b7395bbe561b6a518258c3a5d16
gabascott <smskgbr@gmail.com>
2021-12-09 14:31:51 +0100

    USER-ek megjelenítése

--- script/user.js
+++ script/user.js
@@ -2,13 +2,13 @@ class User{
     constructor(elem, adat){
         this.elem = elem;
         this.adat = adat;
-        this.userId = this.elem.children(".userId");
-        this.userName = this.elem.children(".userName");
-        this.setAdatok();
+        this.id = this.elem.children(".userId");
+        this.name = this.elem.children(".userName");
+        this.setAdatok(adat);
     }
 
     setAdatok(ertekek){
-        this.userId.html(ertekek.userId);
-        this.userName.html(ertekek.userName);
+        this.id.html(ertekek.id);
+        this.name.html(ertekek.name);
     }
 }
